86ba733037a7a34c6796ed5e5d58ad5318fd5b37
Leonardo Do Nascimento Pereira <45684686800@info.local>
2019-04-18 11:27:54 -0300

    Csharp

---
+++ ExPiTsuka/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ExPiTsuka
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool sair = false;
+
+            Usuario[] ussuarios = new Usuario[2];
+
+            do
+            {
+            Console.Clear();
+                System.Console.WriteLine("===================================");
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                System.Console.WriteLine("              PiTsuka      ");
+                Console.ResetColor();
+                System.Console.WriteLine("         Seja bem-vindo(a)         ");
+                System.Console.WriteLine("===================================");
+                System.Console.WriteLine("||      Digite sua opção:        ||");
+                System.Console.WriteLine("||  1 - Cadastrar Usuário        ||");
+                System.Console.WriteLine("||  2 - Efetuar login            ||");
+                System.Console.WriteLine("||  3 - Listar Usuário           ||");
+                System.Console.WriteLine("||  9 - Sair                     ||");
+                System.Console.WriteLine("===================================");
+
+                System.Console.Write ("Código: ");
+
+            int codigo = int.Parse(Console.ReadLine());
+
+            switch(codigo){
+                case 1: 
+                    Usuario usuario = new Usuario();
+
+                    System.Console.WriteLine("Digite seu nome");
+                    usuario.nome = Console.ReadLine();
+
+                break;
+
+                case 2:
+
+                break;
+
+                case 3:
+                
+                break;
+
+                case 9:
+                    sair = true;
+                break;
+            }
+
+
+            } while (!sair);
+        }
+    }
+}
---
+++ ExPiTsuka/Usuario.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ExPiTsuka
+{
+    public class Usuario
+    {
+        public string nome;
+
+        public string email;
+
+        public string senha;
+
+        public DateTime dataCriacao;
+    }
+}
