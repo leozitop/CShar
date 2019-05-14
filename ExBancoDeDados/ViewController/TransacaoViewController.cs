159a6f063914bf93bde9eb816b890379c2118e13
Leonardo Do Nascimento Pereira <45684686800@info.local>
2019-05-14 11:26:35 -0300

    CSharp

--- ExBancoDeDados/ViewController/TransacaoViewController.cs
+++ ExBancoDeDados/ViewController/TransacaoViewController.cs
@@ -7,66 +7,58 @@ using ExBancoDeDados.ViewModel;
 using Spire.Doc;
 using Spire.Doc.Documents;
 
+namespace ExBancoDeDados.ViewController {
+    public class TransacaoViewController {
+        static TransacaoRepositorio transacaoRepositorio = new TransacaoRepositorio ();
 
-
-namespace ExBancoDeDados.ViewController
-{
-    public class TransacaoViewController
-    {
-        static TransacaoRepositorio transacaoRepositorio = new TransacaoRepositorio();
-
-        public static void CadastrarTransacao(){
+        public static void CadastrarTransacao () {
             string tipo;
             double valor = 0;
-            string descricao = Console.ReadLine();
+            string descricao = Console.ReadLine ();
             DateTime dataDaTransacao = DateTime.Now;
+
             double despesa = 0;
-            double receita = 0;
-            
+            double credito = 0;
+            List<TransacaoViewModel> receita = transacaoRepositorio.Listar ();
+
             MenuUtil.MenuTipo();
             tipo = Console.ReadLine();
-            foreach (var item in tipo)
-            {
-                if (tipo.Equals("1"))
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkGreen;
-                    System.Console.WriteLine("Você escolheu RECEITA"); 
-                    Console.ResetColor();
+            foreach (var item in tipo) {
+                if (tipo.Equals("1")) {
+                    foreach (var item in receita) {
+                        if (item != null) {
+                            despesa += item.Valor;
+                        }
+                    }
                     System.Console.WriteLine("Digite o valor da trnsação a ser efetuada:");
-                    valor = double.Parse(Console.ReadLine());
 
-                    receita += valor;
-
-                    do
-                    {
+                    do {
                         Console.WriteLine("Descreva a Transação");
                         descricao = Console.ReadLine();
-                        if (string.IsNullOrEmpty(descricao)){
-                        Console.WriteLine("Descrição Inválida");
-                        }   
+                        if (string.IsNullOrEmpty(descricao)) {
+                            Console.WriteLine("Descrição Inválida");
+                        }
                     } while (string.IsNullOrEmpty(descricao));
 
                     System.Console.WriteLine("A data da sua transação é:");
                     dataDaTransacao = DateTime.Now;
-                    Console.WriteLine(dataDaTransacao);
+                    Console.WriteLine (dataDaTransacao);
                 }
 
-                if (tipo.Equals("2"))
-                {
-                    Console.ForegroundColor = ConsoleColor.DarkRed;
-                    System.Console.WriteLine("Você escolheu DESPESA");
-                    Console.ResetColor(); 
+                if (tipo.Equals("2")) {
+                    foreach (var item in receita) {
+                        if (item != null) {
+                            credito += item.Valor;
+                        }
+                    }
                     System.Console.WriteLine("Digite o valor da trnsação a ser efetuada:");
                     valor = double.Parse(Console.ReadLine());
 
-                    despesa += valor;
-
-                    do
-                    {
+                    do {
                         Console.WriteLine("Descreva a Transação");
                         descricao = Console.ReadLine();
-                        if (string.IsNullOrEmpty(descricao)){
-                        Console.WriteLine("Descrição Inválida");
+                        if (string.IsNullOrEmpty(descricao)) {
+                            Console.WriteLine("Descrição Inválida");
                         }
                     } while (string.IsNullOrEmpty(descricao));
 
@@ -74,47 +66,45 @@ namespace ExBancoDeDados.ViewController
                     Console.WriteLine(dataDaTransacao);
                 }
 
-                if (tipo.Equals("3"))
-                {
-                    Console.WriteLine(receita - despesa);
+                if (tipo.Equals("3")) {
+                    double saldoAtual = credito - despesa;
+                    System.Console.WriteLine(saldoAtual);
                 }
 
-                if (tipo.Equals("0"))
-                {
+                if (tipo.Equals("0")) {
                     //sair
                 }
             }
 
-            TransacaoViewModel transacaoViewModel = new TransacaoViewModel();
+            TransacaoViewModel transacaoViewModel = new TransacaoViewModel ();
             transacaoViewModel.Tipo = tipo;
             transacaoViewModel.Valor = valor;
             transacaoViewModel.Descricao = descricao;
             transacaoViewModel.DataDaTransacao = dataDaTransacao;
 
-            transacaoRepositorio.Inserir(transacaoViewModel);    
+            transacaoRepositorio.Inserir(transacaoViewModel);
 
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Transação cadastrada com êxito");
-            Console.ResetColor();        
-
+            Console.ResetColor();
         }
-        public static void ListarTransacao(){
+
+        public static void ListarTransacao () {
             List<TransacaoViewModel> listaDeTransacaoes = transacaoRepositorio.Listar();
 
-            foreach (var item in listaDeTransacaoes){
-                System.Console.WriteLine($"Tipo: {item.Tipo} Valor: {item.Valor} - Descrição: {item.Descricao} - Data da transação: {item.DataDaTransacao}");
+            foreach (var item in listaDeTransacaoes) {
+                System.Console.WriteLine ($"Tipo: {item.Tipo} Valor: {item.Valor} - Descrição: {item.Descricao} - Data da transação: {item.DataDaTransacao}");
             }
         }
-        
-        public static void Zipar()
-        {
-            string startPath = @".\usuario.zip";
-            string zipPath = @".\usuario.zip";
+
+        public static void Zipar () {
+            string startPath = @".\usuario.csv";
+            string zipPath = @".\banco_de_dados.zip";
             string extractPath = @".\usuario.csv";
 
             ZipFile.CreateFromDirectory(startPath, zipPath);
 
             ZipFile.ExtractToDirectory(zipPath, extractPath);
-        }          
+        }
     }
 }
